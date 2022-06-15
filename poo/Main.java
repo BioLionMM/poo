3ba3263adb84d79eed61304f49cd83a23b17bbfb
BioLionMM <superpikapika@gmail.com>
2022-06-15 14:29:14 +0200

    getters et setters

--- poo/Main.java
+++ poo/Main.java
@@ -30,7 +30,7 @@ public class Main {
         System.out.println("La longueur : "+parcelle.longueur);
         System.out.println("La largeur : "+parcelle.largeur);*/
 
-        Personne exemplePersonne=new Personne();
+        /*Personne exemplePersonne=new Personne();
         Scanner input=new Scanner(System.in);
 
         System.out.println("Entrez nom, prenom, pays, true si vous etes marié, false sinon, puis nombre d'enfants séparés par un retour à la ligne: ");
@@ -40,7 +40,76 @@ public class Main {
         exemplePersonne.married=input.nextBoolean();
         exemplePersonne.nombreEnfants=input.nextInt();
 
-        exemplePersonne.getInfos();
+        exemplePersonne.getInfos();*/
+
+        //        BEFORE GETTERS AND SETTERS
+//        System.out.println("***********");
+//        System.out.println("**Objet 1**");
+//        System.out.println("************\n");
+//        Rectangle terrain=new Rectangle();
+//        System.out.println("Etat inital de l'objet après " +
+//                "création\n======================");
+//        System.out.println("La longueur : "+terrain.longueur);
+//        System.out.println("La largeur : "+terrain.largeur);
+//
+//        System.out.println("Etat de l'objet après avoir fourni des " +
+//                "valeur\n==========================");
+//        terrain.longueur=500;
+//        terrain.largeur=350;
+//        System.out.println("La longueur : "+terrain.longueur);
+//        System.out.println("La largeur : "+terrain.largeur);
+//        System.out.println("Surface : "+terrain.calculDeLaSurface());
+//        System.out.println("Perimète : "+terrain.calculDuPerimetre());
+//
+//
+//        System.out.println("***********");
+//        System.out.println("**Objet 2**");
+//        System.out.println("************\n");
+//        Rectangle parcelle=new Rectangle();
+//        System.out.println("La longueur : "+parcelle.longueur);
+//        System.out.println("La largeur : "+parcelle.largeur);
+
+
+//        AFTER ADDING GETTERS AND SETTERS
+//        System.out.println("***********");
+//        System.out.println("**Objet 1**");
+//        System.out.println("************\n");
+//        Rectangle2 terrain=new Rectangle2();
+//        System.out.println("Etat inital de l'objet après " +
+//                "création\n======================");
+//        System.out.println("La longueur : "+terrain.getLongueur());
+//        System.out.println("La largeur : "+terrain.getLargeur());
+//
+//        System.out.println("Etat de l'objet après avoir fourni des " +
+//                "valeur\n==========================");
+//        terrain.setLongueur(500);
+//        terrain.setLargeur(350);
+//        System.out.println("La longueur : "+terrain.getLongueur());
+//        System.out.println("La largeur : "+terrain.getLargeur());
+//        System.out.println("Surface : "+terrain.calculDeLaSurface());
+//        System.out.println("Perimète : "+terrain.calculDuPerimetre());
+
+
+
+//      Ajout d'un constructeur
+        System.out.println("***********");
+        System.out.println("**Objet 1**");
+        System.out.println("************\n");
+        Rectangle3 terrain=new Rectangle3(500,350);
+        System.out.println("Etat inital de l'objet après " +
+                "création\n======================");
+        System.out.println("La longueur : "+terrain.getLongueur());
+        System.out.println("La largeur : "+terrain.getLargeur());
+
+        System.out.println("Etat de l'objet après avoir fourni des " +
+                "valeur\n==========================");
+//        terrain.setLongueur(500);
+//        terrain.setLargeur(350);
+//        System.out.println("La longueur : "+terrain.getLongueur());
+//        System.out.println("La largeur : "+terrain.getLargeur());
+//        System.out.println("Surface : "+terrain.calculDeLaSurface());
+//        System.out.println("Perimète : "+terrain.calculDuPerimetre());
+
     }
 
 }
